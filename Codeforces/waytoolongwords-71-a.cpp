b0081a11ede7a640b83d18c2e01c366e39cf95c4
Yash Suroshe <yashsuroshe230541@acropolis.in>
2026-03-02 17:41:51 +0530

    Added new problem

---
+++ Codeforces/waytoolongwords-71-a.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    string s;
+    while(n--)
+    {
+        cin>>s;
+    if(s.length()>10)
+    {
+        cout<<s[0]<<s.length()-2<<s[s.length()-1];
+    }
+    else
+    {
+        cout<<s;
+    }
+}
+    return 0;
+}
