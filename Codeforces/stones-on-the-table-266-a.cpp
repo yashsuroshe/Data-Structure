bce43e18c72c124330efe3118ed3872a3946d2cb
Yash Suroshe <yashsuroshe230541@acropolis.in>
2026-03-07 21:57:45 +0530

    Codeforces Submission

---
+++ Codeforces/stones-on-the-table-266-a.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int ans=0;
+    for(int i=1;i<n;i++){
+        if(s[i]==s[i-1]) ans++;
+    }
+    cout<<ans;
+    return 0;
+}
